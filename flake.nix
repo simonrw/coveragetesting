2aa591f6a763b99bca8280551d374ae24aedcf6b
Simon Walker <s.r.walker101@googlemail.com>
2023-04-12 14:00:50 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Flake utils demo";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+        ];
+
+        pkgs = import nixpkgs {
+          inherit overlays system;
+        };
+      in
+      {
+        devShells = rec {
+          default = python-dev;
+
+          python-dev = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              pkgs.python3
+              pkgs.python3.pkgs.venvShellHook
+            ];
+
+            venvDir = ".venv";
+
+            postVenvCreation = ''
+              python -m pip install pip-tools
+            '';
+          };
+        };
+      }
+    );
+}
