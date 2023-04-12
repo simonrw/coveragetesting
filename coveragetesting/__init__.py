a10208588661cfb6093d99252819076840189c3a
Simon Walker <s.r.walker101@googlemail.com>
2023-04-12 14:06:28 +0100

    Add package

---
+++ coveragetesting/__init__.py
@@ -0,0 +1,5 @@
+def a():
+    return 10
+
+def b():
+    return 20
---
+++ tests/test_foo.py
@@ -0,0 +1,13 @@
+import coveragetesting
+
+
+def test_a():
+    assert coveragetesting.a() == 10
+
+
+def test_b():
+    assert coveragetesting.b() == 20
+
+
+def test_a_again():
+    assert coveragetesting.a() == 10
